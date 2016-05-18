142d02d2f1e65ca2930ba2994b529db6fa558660
justitow <justitow@gmail.com>
2016-05-18 14:50:34 -0700

    Lots of changes, probably lots of broken

--- MemMan.cpp
+++ MemMan.cpp
@@ -121,6 +121,7 @@ void MemSpace::remove(int adr)
 
 void MemSpace::print()
 {
+  /*
   curr = head;
   if (curr == NULL)
     return;
@@ -131,6 +132,7 @@ void MemSpace::print()
     curr = curr->next;
   }
   cout << curr->address << endl;
+   */
 }
 
 Process::Process()
