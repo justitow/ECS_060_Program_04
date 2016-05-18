3399c2646ca2183bac1d817d24a6f91f38d449be
justitow <justitow@gmail.com>
2016-05-18 14:49:30 -0700

    Lots of changes, probably lots of broken

--- MemMan.cpp
+++ MemMan.cpp
@@ -46,7 +46,7 @@ void MemSpace::insert(int adr, int size)
   }
   else
   {
-    for (curr = head; curr->next != NULL && adr < curr->address; curr = curr->next)
+    for (curr = head; curr->next != NULL && adr > curr->address; curr = curr->next)
       prev = curr;
 
     if (curr == head)
