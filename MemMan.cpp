733c1b72b47baf6bb94998bfaeb0a0c54dd5b333
justitow <justitow@gmail.com>
2016-05-18 13:10:07 -0700

    Lots of changes, probably lots of broken

--- MemMan.cpp
+++ MemMan.cpp
@@ -223,6 +223,7 @@ void MemMan::endProc(int proc, int opNum, MemCheck &memCheck, char print)
   // free all memory assigned to proc.
   while (this->processes[proc].space->head->next != NULL)
   {
+    cout << "Ending: " << this->processes[proc].space->head->address << end;
     memCheck.deAlloc(proc, this->processes[proc].space->head->address, opNum);
     this->processes[proc].space->head = this->processes[proc].space->head->next;
     if (this->processes[proc].space->head->prev != NULL)
