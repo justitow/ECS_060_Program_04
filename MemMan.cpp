4fcbeae72d4a1bdb11ccfa8c7259a88a00e974c0
justitow <justitow@gmail.com>
2016-05-18 12:40:53 -0700

    Lots of changes, probably lots of broken

--- MemMan.cpp
+++ MemMan.cpp
@@ -46,11 +46,9 @@ void MemSpace::insert(int adr, int size)
   else
   {
     for (curr = head; curr->next != NULL && curr->address < adr; prev = curr, curr = curr->next)
-      cout << "forward" << endl;
 
     if (curr == head)
     {
-      cout << "head insert" << endl;
       if (adr < curr->address)
       {
         myblock->next = head;
@@ -66,7 +64,6 @@ void MemSpace::insert(int adr, int size)
 
     else if (curr->next == NULL)
     {
-      cout << "tail insert" << endl;
       if (adr < curr->address)
       {
         curr->prev->next = myblock;
@@ -83,7 +80,6 @@ void MemSpace::insert(int adr, int size)
 
     else
     {
-      cout << "mid insert" << endl;
       curr->prev->next = myblock;
       myblock->prev = curr->prev;
       myblock->next = curr;
