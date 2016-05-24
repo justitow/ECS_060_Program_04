28bcbbf300608596aea842f9e6efd72e0698e451
justitow <justitow@gmail.com>
2016-05-23 19:05:51 -0700

    works?

--- MemMan.cpp
+++ MemMan.cpp
@@ -117,9 +117,6 @@ bool MemSpace::check_for_adr(int adr)
   {
     for (curr = head; curr->next != NULL && curr->address < adr; curr = curr->next);
 
-    cout << "Last: " << this->last->address << endl;
-    cout << "Started at: " << curr->address << endl;
-
     if (curr != head)
     {
       if (curr != last || (curr == last && adr < curr->address))
@@ -130,12 +127,10 @@ bool MemSpace::check_for_adr(int adr)
 
     if ((adr <= curr->address + curr->block_size - 1) && (adr >= curr->address))
     {
-      cout << curr->address << endl;
       return true;
     }
   }
 
-  cout << curr->address << endl;
   return false;
 }
 
